e8e8ff27e9b9c53f13271706e8c216c4e0f70032
sergioLahuerta <64411350+sergioLahuerta@users.noreply.github.com>
2025-10-22 12:16:35 +0200

    Hechos U3.2 y U3.3 enteros

--- UD3/02_custom_filter/main.js
+++ UD3/02_custom_filter/main.js
@@ -1,15 +1,89 @@
 const assert = require('assert').strict;
 
-function select(array, condition) {
+//SOLO PARES
+function pairs(num){
+    if(num %2 === 0) {
+        return true
+    } else{
+        return false
+    }
 }
 
-let values = [1, 2, 3, 5, 7, 13, 17, 23, 29]
+//Mayores a 15
+function gt15(num) {
+    if(num > 15) {
+        return true
+    } else{
+        return false
+    }
+}
 
-// sólo pares
-assert.deepStrictEqual(select(values, pairs), [2])
+//Menores a 10
+function lt10(num) {
+    if(num < 10) {
+        return true
+    } else{
+        return false
+    }
+}
+
+function selectPairs(array, condition) {
+    let array2 = [];
+    array.forEach(value => {
+        if(condition(value)){
+            array2.push(value)
+            return true
+        }else {
+            return false
+        }
+    });
+    return array2
+}
+
+
+function selectGt15(array, condition) {
+    //MAYORES QUE 15
+    let array21 = []
+    array.forEach(value => {
+        if(condition(value)) {
+            array21.push(value)
+            return true
+        } else{
+            return false
+        }
+    })
+    return array21
+}
+
+
+function selectLt10(array, condition) {
+    //MENORES A 10
+    let array2 = []
+    array.forEach(value => {
+        if(condition(value)) {
+            array2.push(value)
+            return true
+        } else{
+            return false
+        }
+    })
+    return array2
+}
+
+
+let values = [1, 2, 3, 4, 5, 7, 13, 17, 23, 29, 88, 6]
+
+// Solo pares
+// assert.deepStrictEqual(select(values, pairs), [2, 4])
+// select(values, pairs)
+// console.log('Inicio del programa')
+// console.log('Array ', values)
+console.log('Pares', selectPairs(values, pairs))
 
 // mayores que 15
-assert.deepStrictEqual(select(values, gt15), [17, 23, 29])
+// assert.deepStrictEqual(select(values, gt15), [17, 23, 29])
+console.log('Números mayores a 15 -> ', selectGt15(values, gt15))
 
 // menores de 10
-assert.deepStrictEqual(select(values, lt10), [1, 2, 3, 5, 7])
+// assert.deepStrictEqual(select(values, lt10), [1, 2, 3, 5, 7])
+console.log('Números menores a 10 -> ', selectLt10(values, lt10))
--- UD3/03_arrow_custom_filter/main.js
+++ UD3/03_arrow_custom_filter/main.js
@@ -1,15 +1,54 @@
 const assert = require('assert').strict;
 
-function select(array, condition) {
+const pairs = num => num %2 == 0
+
+function selectPairs(array, condition) {
+    let array2 = []
+    array.forEach(element => {
+        if(condition(element)){
+            array2.push(element)
+        }
+    });
+    return array2
 }
 
-let values = [1, 2, 3, 5, 7, 13, 17, 23, 29]
+const gt15 = num => num > 15 
+
+function selectGt15(array, condition) {
+    let array2 = []
+    array.forEach(element => {
+        if(condition(element)){
+            array2.push(element)
+            return true
+        }
+    });
+    return array2
+}
+
+const lt10 = num => num < 10
+
+function selectLt10 (array, condition) {
+    let array2 = []
+    array.forEach(element => {
+        if(condition(element)) {
+            array2.push(element)
+            return true
+        }
+    });
+    return array2
+}
+
+let values = [1, 2, 3, 4, 5, 7, 8, 13, 17, 23, 29, 33]
+
+console.log('Números pares: ', selectPairs(values, pairs))
+console.log('Números mayores a 15: ', selectGt15(values, gt15))
+console.log('Números menores a 10: ', selectLt10(values, lt10))
 
 // sólo pares
-assert.deepStrictEqual(select(values, pairs), [2])
+// assert.deepStrictEqual(select(values, pairs), [2])
 
 // mayores que 15
-assert.deepStrictEqual(select(values, gt15), [17, 23, 29])
+// assert.deepStrictEqual(select(values, gt15), [17, 23, 29])
 
 // menores de 10
-assert.deepStrictEqual(select(values, lt10), [1, 2, 3, 5, 7])
+// assert.deepStrictEqual(select(values, lt10), [1, 2, 3, 5, 7])
